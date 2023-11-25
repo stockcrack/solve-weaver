40446132365d3ec673da747f7ba7cf8a5e343ef8
Harley Davis <harley@alum.mit.edu>
2023-11-25 09:13:18 +0100

    Refactor word graph creation and storage
    Refactored the code to improve efficiency and readability in creating
    and storing word graphs for different word lengths. Instead of separate
    variables for each word graph, the refactored code now utilizes a
    dictionary to store word graphs based on word length. This change
    simplifies the logic and reduces redundancy. Additionally, the word
    graph creation process now utilizes a `defaultdict(list)` to optimize
    performance. This change enhances the performance of the word graph
    generation process. These changes ensure that the code is more
    maintainable and scalable. No known issues or consequences.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import solve_weaver as sw
 from typing import Union, Annotated, Any
 from pydantic import BaseModel
 import sys
+from collections import defaultdict
 
 app = FastAPI()
 
@@ -36,13 +37,16 @@ def read_words(words: set, filename: str):
 
 # Create word list
 def create_word_graph(words, excluded_words):
-    return sw.build_word_graph(words - excluded_words)
+    return sw.build_word_graph(words - excluded_words, defaultdict(list))
 
 words4 = read_words(set(), 'four_letter_words.txt')
 words5 = read_words(set(), 'five_letter_words.txt')
 excluded_words = read_words(set(), 'excluded_words.txt')
 word_graph4 = create_word_graph(words4, excluded_words)
+print(f"Word_graph4 length is {len(word_graph4)}")
 word_graph5 = create_word_graph(words5, excluded_words)
+print(f"Word_graph5 length is {len(word_graph5)}")
+word_graphs = { 4: word_graph4, 5: word_graph5}
 
 print(f"Python version: {sys.version}")
 
@@ -51,17 +55,15 @@ def getwordladder(start: str, end: str) -> list[str] | None:
     if len(start) != len(end):
         print(f"Length of {start} does not match length of {end}")
         return None
-    word_graph = None
     if len(start) == 4:
         print("4 letters")
-        word_graph = word_graph4
     elif len(start) == 5:
         print("5 letters")
-        word_graph = word_graph5
     else:
         print("Only 4 or 5 letter words accepted at this time.")
         return None
 
+    word_graph = word_graphs[len(start)]
     print(f"Word graph length is {len(word_graph)}")
     path = sw.word_ladder(start, end, word_graph)
     print(f"Path from {start} to {end} is {path}.")
@@ -87,8 +89,10 @@ async def exclude_word(word: Annotated[str, Path(min_length=4, max_length=5, tit
     else:
         print(f"Excluding {word}")
         excluded_words.add(word)
-        word_graph4 = sw.build_word_graph(words4 - excluded_words)
-        word_graph5 = sw.build_word_graph(words5 - excluded_words)
+        word_graph4 = sw.build_word_graph(words4 - excluded_words, defaultdict(list))
+        word_graph5 = sw.build_word_graph(words5 - excluded_words, defaultdict(list))
+        word_graphs[4] = word_graph4
+        word_graphs[5] = word_graph5
         with(open("excluded_words.txt",'a')) as f:
             print(word, file=f)
         return word
